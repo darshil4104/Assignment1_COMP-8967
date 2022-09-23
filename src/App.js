3f0712c078310520fa5b1ebfaaf779be6c56849a
Zeel Patel <zeel.c.patel@gmail.com>
2022-09-23 15:08:00 -0400

    converted to function components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from "react";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/login";
-import SignUp from "./components/signup";
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Login from './components/login'
+import SignUp from './components/signup'
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <div className="auth-wrapper">
-          <div className="auth-inner">
-            <Routes>
-              <Route exact path="/" element={<Login />} />
-              <Route path="/signin" element={<Login />} />
-              <Route path="/signup" element={<SignUp />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<div className="auth-wrapper">
+					<div className="auth-inner">
+						<Routes>
+							<Route exact path="/" element={<Login />} />
+							<Route path="/signin" element={<Login />} />
+							<Route path="/signup" element={<SignUp />} />
+						</Routes>
+					</div>
+				</div>
+			</div>
+		</Router>
+	)
 }
-export default App;
+export default App
