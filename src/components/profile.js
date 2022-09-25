021637be6c98d22fff6c3607de836ec9459b0fc1
Avleen Kaur Mehna <avleenkaurmehna@gmail.com>
2022-09-25 19:08:50 -0400

    Add logout functionality to profile page

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,47 +1,56 @@
 import React, { useState } from 'react';
 import "./profile.css";
+import { Link, useNavigate } from "react-router-dom";
 
- function Profile() {
+function Profile() {
+  let navigate = useNavigate();
 
-    const name ="Hetvi Patel";
-    const phone = "12345679";
-    const email="txt@mail.com";
-    const profileImg = "http://gravatar.com/avatar/288ce55a011c709f4e17aef7e3c86c64?s=200";
-    const bio = "ljasdjkaskd";
+  const name = "Hetvi Patel";
+  const phone = "12345679";
+  const email = "txt@mail.com";
+  const profileImg =
+    "http://gravatar.com/avatar/288ce55a011c709f4e17aef7e3c86c64?s=200";
+  const bio = "ljasdjkaskd";
 
-    
+  const logout = () => {
+    localStorage.removeItem("AuthToken");
+    localStorage.removeItem("email");
+    localStorage.removeItem("isSocialLogin");
+    navigate(`/signin`);
+  };
+
   return (
-    <><div class="content-profile-page">
-          <div>
-            
-              <div class="img-user-profile">
-                  <img class="avatar" src={profileImg} alt="profile" />
-              </div>
-              <div class="user-profile-data">
-                  <h1>{name}</h1>
-                  <p>Phone: {phone}</p>
-                  <p>Email: {email}</p>
-              </div>
-              <div class="description-profile">Bio: {bio}</div>
-              <br></br>
-              <br></br>
-            <div class="edit">
-            <button class='btn-btn-primary'>
-                <a href='/edit-profile' style={{
-                    color:'black'
-                }}>
-
-            Edit
-
-                </a>
-
-            </button>
-            </div>
-
-           
+    <>
+      <div className="content-profile-page">
+        <div>
+          <div className="img-user-profile">
+            <img className="avatar" src={profileImg} alt="profile" />
           </div>
-
+          <div className="user-profile-data">
+            <h1>{name}</h1>
+            <p>Phone: {phone}</p>
+            <p>Email: {email}</p>
+          </div>
+          <div className="description-profile">Bio: {bio}</div>
+          <br></br>
+          <br></br>
+          <div className="d-grid">
+            <a
+              href="/edit-profile"
+              className="d-grid"
+              style={{
+                color: "black",
+                textDecoration: "none",
+              }}
+            >
+              <button className="btn btn-primary">Edit</button>
+            </a>
+            <button className="btn btn-primary mt-2" onClick={logout}>
+              Logout
+            </button>
+          </div>
+        </div>
       </div>
-      </>
+    </>
   );
 }export default Profile;
